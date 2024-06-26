1c6b2128e459456d929a8b2bf31a42e798564207
SarojiniValliyappan <sarojiniv@clanizon.com>
2024-06-26 16:00:46 +0530

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/components/BasicCodes.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <header>
+        <img src=""/>
+      </header>
+     
+    </div>
+  )
+}
+
+export default App
